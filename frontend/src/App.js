859a6a200191112040c6c0466b965190326d1a8c
Mujtaba Anwar Shaikh <shaikhmujtaba072@gmail.com>
2025-10-02 17:46:37 +0530

    added to github

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '50px', fontFamily: 'Arial' }}>
+      <h1>Notes App Demo</h1>
+      <textarea placeholder="Write your notes here..." style={{ width: '100%', height: '200px' }}></textarea>
+    </div>
+  );
+}
+
+export default App;
